55b72d2b3cc7a454ec2be3551c8f8cc68b2b975e
Sasha4907 <aleksaped@gmail.com>
2022-08-06 21:37:23 +0300

    make it better

--- index.js
+++ index.js
@@ -8,14 +8,6 @@ const logger = require('./winston');
 
 app.use(express.json({ extended: true }));
 
-if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static('client/build'));
-}
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-
 app.use('/api/auth', require('./routes/AuthRoutes'));
 app.use('/api/link', require('./routes/LinkRoutes'));
 app.use('/api/admin', require('./routes/AdminRoutes'));

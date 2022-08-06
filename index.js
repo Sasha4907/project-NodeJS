9c89a7575ffc70e9d7698f4c9a126e5667bdefc0
Sasha4907 <aleksaped@gmail.com>
2022-08-06 18:40:28 +0300

    make it better

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const logger = require('./winston');
 
 app.use(express.json({ extended: true }));
 
-if(process.env.NODE_ENV === 'production'){
-  //set static folder
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
   app.use(express.static('client/build'));
 }
-app.get('*',(req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 

983a9d91327b561978c69f3106e755a540c0184e
Sasha4907 <aleksaped@gmail.com>
2022-08-16 18:45:20 +0300

    deploy

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 const app = express();
 const logger = require('./winston');
 
 app.use(express.json({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use('/api/auth', require('./routes/AuthRoutes'));
 app.use('/api/link', require('./routes/LinkRoutes'));
